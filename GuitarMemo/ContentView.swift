36fb459d4f0d87fd5e3e75fb9bd5f52169a22e60
Seobe95 <woodyms@nate.com>
2024-03-25 16:58:48 +0900

    Initial Commit

---
+++ GuitarMemo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  GuitarMemo
+//
+//  Created by 이명섭 on 3/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ GuitarMemo/GuitarMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarMemoApp.swift
+//  GuitarMemo
+//
+//  Created by 이명섭 on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GuitarMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
